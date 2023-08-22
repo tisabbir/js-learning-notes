23bc02cbab64f47853853bbf6864b645f663f78d
MD Tarekul Islam Sabbir <tisabbir0@gmail.com>
2023-08-22 16:23:54 +0600

    second commit

---
+++ image slider project/index.js
@@ -0,0 +1,29 @@
+var photos = ["images/1.jpg", "images/2.jpg", "images/3.jpg", ]
+var imgTag = document.querySelector("img");
+
+var count = 0;
+function next(){
+   count++;
+   
+   if (count >= photos.length){
+    count = 0;
+    imgTag.src = photos[count];
+   }
+   else{
+    imgTag.src = photos[count];
+   }
+
+}
+
+function prev(){
+count--;
+
+if (count <0){
+    count=photos.length-1;
+    imgTag.src = photos[count];
+}
+else{
+    imgTag.src = photos[count];
+   }
+
+}
