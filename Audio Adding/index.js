0d9a8ef2d6fd14c066cfa7bd1e803e855cdf27b6
MD Tarekul Islam Sabbir <tisabbir0@gmail.com>
2023-08-27 11:25:44 +0600

    animation added

--- Audio Adding/index.js
+++ Audio Adding/index.js
@@ -2,6 +2,7 @@ for(i=0; i<3; i++){
     document.querySelectorAll(".but")[i].addEventListener("click",function(){
         var text = this.innerHTML;
         audioPlay(text);
+        playAnimation(text);
     })
 }
 
@@ -22,4 +23,13 @@ function audioPlay(text){
             audio.play();
             break;
     }
+}
+
+function playAnimation(text){
+   var selected =  document.querySelector("."+text);
+    selected.classList.add("anim");
+
+    setTimeout(function(){
+        selected.classList.remove("anim");
+    }, 2000);
 }
