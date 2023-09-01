100dbe055d4218146585ba4745d5743585dea306
MD Tarekul Islam Sabbir <tisabbir0@gmail.com>
2023-09-01 10:48:51 +0600

    best practices added

---
+++ Best Practice/index.js
@@ -0,0 +1,14 @@
+/*
+Best Practices
+
+use camelCase for naming;
+variable declaration on top and initialize;
+avoid unnecessary variables;
+switch, case, default
+object adn array with const
+== vs === 
+access dom less
+
+
+
+*/
