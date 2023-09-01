55551b5cd544b8780a16cd03eb9243d7e4ad8d37
MD Tarekul Islam Sabbir <tisabbir0@gmail.com>
2023-09-01 07:00:54 +0600

    clipboard event added

---
+++ Clipboard event/index.js
@@ -0,0 +1,11 @@
+const input = document.querySelector("input");
+
+input.addEventListener("copy", function(){
+    console.log("you have copied");
+});
+input.addEventListener("cut", function(){
+    console.log("you have cut");
+});
+input.addEventListener("paste", function(){
+    console.log("you have pasted");
+});
