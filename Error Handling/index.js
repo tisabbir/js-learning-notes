fe0d3d366a332a576cbd95dd4e886af243b12d5a
MD Tarekul Islam Sabbir <tisabbir0@gmail.com>
2023-09-03 13:17:00 +0600

    try, catch, finally added

---
+++ Error Handling/index.js
@@ -0,0 +1,22 @@
+//try, catch,finally, throw
+try{
+    //code test
+    alert("HI everyone");
+    // alert(x);
+    alert("Bye");
+}
+catch(err){
+    //handle error
+    alert("there is error in the code");
+    console.log(err);
+    // console.log(err.name);
+    // console.log(err.massage);
+}finally{
+    alert("Bye Everyone")
+}
+
+
+
+// alert("HI everyone");
+// alert(x); //the error is here
+// alert("Bye");
