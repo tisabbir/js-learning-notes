4f994c8616906a8cc67a1adcf72115d497e51924
MD Tarekul Islam Sabbir <tisabbir0@gmail.com>
2023-09-04 16:39:44 +0600

    es6 basic

---
+++ ES6/index.js
@@ -0,0 +1,13 @@
+//difference between var, let and const
+//string concatination
+//how to declare function in es6
+let x  ="anisul ";
+let y = "islam";
+
+console.log(`x +  y = ${x + y}`);
+
+const add = (x,y) => {
+    let sum = x+y;
+    console.log(sum);
+}
+add(20,30);
